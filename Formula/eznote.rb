a487fdcc605f7998f9e28ab0e1745255bd3c0099
Amritesh Anand <amriteshanand7@gmail.com>
2026-01-02 23:02:41 -0500

    updating binaries

--- Formula/eznote.rb
+++ Formula/eznote.rb
@@ -1,7 +1,9 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
 class Eznote < Formula
   desc "Zero-friction note taking for developers who live in the terminal"
   homepage "https://github.com/amritessh/eznote"
-  version "0.0.1"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
@@ -10,7 +12,7 @@ class Eznote < Formula
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5  -"
     else
       url "https://github.com/amritessh/eznote/releases/download/v0.1.0/ezn-macos-x86_64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5  -"  
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5  -"
     end
   end
 
@@ -20,11 +22,12 @@ class Eznote < Formula
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5  -"
     else
       url "https://github.com/amritessh/eznote/releases/download/v0.1.0/ezn-linux-x86_64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5  -"  
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5  -"
     end
   end
 
   def install
+    # Determine the correct binary name based on platform
     binary = if OS.mac?
       if Hardware::CPU.arm?
         "ezn-macos-aarch64"
@@ -43,6 +46,14 @@ class Eznote < Formula
   end
 
   test do
+    # Test that the binary runs and shows version
     assert_match "ezn", shell_output("#{bin}/ezn --version")
+    
+    # Test adding a note
+    system "#{bin}/ezn", "add", "Test note from Homebrew"
+    
+    # Test listing notes
+    output = shell_output("#{bin}/ezn list")
+    assert_match "Test note from Homebrew", output
   end
 end
